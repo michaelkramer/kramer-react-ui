2a985c90b277f4be966e8c8ddd59a06c631e1662
Michael Kramer <michaelrkramer@gmail.com>
2020-10-16 19:39:59 -0400

    my icon

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,6 @@
-// import * as React from "react";
-// import styles from "./styles.module.css";
 import Icons from "./components/Icons";
 import { IconTheme, IconThemes } from "./components/Icons/IconTheme";
 import IconList from "./components/Icons/IconList";
 
-// interface Props {
-//   text: string;
-// }
-
-// export const ExampleComponent = ({ text }: Props) => {
-//   return <div className={styles.test}>Example! Component: {text}
-
-//     <Icons theme="antd" name="Home" />
-//   </div>;
-// };
-// export { $IconObject, $Icon };
 export { Icons, IconTheme, IconThemes, IconList };
 
